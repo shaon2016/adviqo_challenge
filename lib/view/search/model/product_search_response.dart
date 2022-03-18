057f69cd649f39ec2ba549e7204f0fcf94b12368
Md Ashiqul Islam <shaondeveloper@gmail.com>
2022-03-18 23:44:36 +0600

    price double int exception solved

--- lib/view/search/model/product_search_response.dart
+++ lib/view/search/model/product_search_response.dart
@@ -3,18 +3,21 @@ class ProductSearchResponse {
     required this.paging,
     required this.results,
   });
+
   late final Paging paging;
   late final List<ProductSearchResults> results;
 
-  ProductSearchResponse.fromJson(Map<String, dynamic> json){
+  ProductSearchResponse.fromJson(Map<String, dynamic> json) {
     paging = Paging.fromJson(json['paging']);
-    results = List.from(json['results']).map((e)=>ProductSearchResults.fromJson(e)).toList();
+    results = List.from(json['results'])
+        .map((e) => ProductSearchResults.fromJson(e))
+        .toList();
   }
 
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
     _data['paging'] = paging.toJson();
-    _data['results'] = results.map((e)=>e.toJson()).toList();
+    _data['results'] = results.map((e) => e.toJson()).toList();
     return _data;
   }
 }
@@ -26,12 +29,13 @@ class Paging {
     required this.offset,
     required this.limit,
   });
+
   late final int total;
   late final int primaryResults;
   late final int offset;
   late final int limit;
 
-  Paging.fromJson(Map<String, dynamic> json){
+  Paging.fromJson(Map<String, dynamic> json) {
     total = json['total'];
     primaryResults = json['primary_results'];
     offset = json['offset'];
@@ -55,15 +59,20 @@ class ProductSearchResults {
     required this.price,
     required this.thumbnail,
   });
+
   late final String id;
   late final String title;
-  late final int price;
+  late final double price;
   late final String thumbnail;
 
-  ProductSearchResults.fromJson(Map<String, dynamic> json){
+  ProductSearchResults.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
-    price = json['price'];
+    price = json['price'] != null
+        ? json['price'] is int
+            ? json['price'].toDouble()
+            : json['price']
+        : 0.0;
     thumbnail = json['thumbnail'];
   }
 
